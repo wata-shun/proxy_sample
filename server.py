ad044c2197774978ebba9c1148ccdcce7578c88b
wata-shun <51885135+wata-shun@users.noreply.github.com>
2019-12-20 17:12:55 +0900

    Add files via upload

--- server.py
+++ server.py
@@ -1,24 +1,19 @@
 # socket サーバを作成
-
 import socket
 
-# AF = IPv4 という意味
-# TCP/IP の場合は、SOCK_STREAM を使う
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # IPアドレスとポートを指定
-    s.bind(('127.0.0.1', 50007))
-    # 1 接続
-    s.listen(1)
-    # connection するまで待つ
+#アドレス、ポートの設定
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.bind(('127.0.0.1', 50007))
+soc.listen(1)
+
+while True:
+    #誰かがアクセスしてきたら、コネクションを確立
+    conn, addr = soc.accept()
     while True:
-        # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
-        conn, addr = s.accept()
-        with conn:
-            while True:
-                # データを受け取る
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print('data:{}\naddr:{}'.format(data, addr))
-                # クライアントにデータを返す(b -> byte でないといけない)
-                conn.sendall(b'Received_im_server ' + data)
+        #データを受け取る
+        data = conn.recv(1024)
+        if not data:
+            break
+        print('data:{}\naddr:{}'.format(data, addr))
+        #受け取れたら送信元に返す
+        conn.sendall(b'Received ' + data)
